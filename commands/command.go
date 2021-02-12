554685fca49428e0953314956f963026ed570d2e
Matthew Hall <matthew.hall@partnerize.com>
2021-02-12 15:16:21 -0500

    doc blocks

--- commands/command.go
+++ commands/command.go
@@ -7,7 +7,12 @@ import (
 )
 
 type Command interface {
+	// GetShortcut returns the command keyword. i.e '/help'
 	GetShortcut() string
+
+	// RegisterHandler binds the command to the session
 	RegisterHandler(ctx telnet.Context, name string, args ...string) telsh.Handler
+
+	// runCommand Performs the command
 	runCommand(stdin io.ReadCloser, stdout io.WriteCloser, stderr io.WriteCloser, args ...string) error
 }
